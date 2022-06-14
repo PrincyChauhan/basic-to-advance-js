ab7633a3bab74c7758df8e9d6ac39833d4c2e0b8
PrincyChauhan <chauhanprincee7@gmail.com>
2022-06-14 12:31:12 +0530

    Introduction

---
+++ intoduction/variable.js
@@ -0,0 +1,24 @@
+// Declaring different variables of different data types
+
+let firstName = 'Princy'    // first name of a person
+let lastName = 'Chauhan'      // last name of a person
+let country = 'India'       // country
+let city = 'Bhuj'         // capital city
+let age =   22               // age in years
+
+
+// Declaring variables with number values
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'Princy',     //name of a person
+  job = 'Web-devloper',
+  live = 'Ahmedabad'
+
+  console.log(firstName, lastName, country, city, age);
+console.log(gravity, boilingPoint, PI); // 9.81, 100, 3.14
+console.log(name, job, live)
