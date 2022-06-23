57983b3dfae05ec931ee85224b6cc8093c579a14
PrincyChauhan <chauhanprincee7@gmail.com>
2022-06-23 10:31:50 +0530

    loop

---
+++ Loops/do_while_loop.js
@@ -0,0 +1,5 @@
+let j = 0
+do {
+  console.log(j)
+  i++
+} while (j <= 5)
---
+++ Loops/for_loop.js
@@ -0,0 +1,19 @@
+
+
+const countries = ['India', 'Pakistan', 'China', 'Norway', 'Us']
+const newArr = []
+for(let i = 0; i < countries.length; i++){
+  newArr.push(countries[i].toUpperCase())
+}
+console.log(newArr)
+
+
+
+// const numbers = [1, 2, 3, 4, 5]
+// let sum = 0
+// for(let i = 0; i < numbers.length; i++){
+//   sum  = sum + numbers[i]  // can be shorten, sum += numbers[i]
+
+// }
+
+// console.log(sum)
